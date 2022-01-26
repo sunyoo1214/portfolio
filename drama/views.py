57e25a6a77da3c7e4988d6ae8ce5f542315429bc
sunyoo1214 <sunyoo1214@gmail.com>
2022-01-26 13:53:19 +0900

    2장완료

---
+++ drama/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from .forms import QuestionForm, AnswerForm
+from .models import Question
+
+
+def index(request):
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+    return render(request, 'drama/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'drama/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('drama:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'drama/question_detail.html', context)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('drama:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'drama/question_form.html', context)
