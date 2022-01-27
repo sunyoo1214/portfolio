ecb801f3d77a4614cbc6937afba974b88057fae8
sunyoo1214 <sunyoo1214@gmail.com>
2022-01-27 20:33:13 +0900

    질문 삭제 기능 보완

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dramasite',
-        'USER': 'sbsst',
-        'PASSWORD': 'sbs123414',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
--- drama/views.py
+++ drama/views.py
@@ -59,6 +59,7 @@ def question_create(request):
     context = {'form': form}
     return render(request, 'drama/question_form.html', context)
 
+
 @login_required(login_url='common:login')
 def question_modify(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -70,6 +71,7 @@ def question_modify(request, question_id):
         form = QuestionForm(request.POST, instance=question)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.modify_date = timezone.now()  # 수정일시 저장
             question.save()
             return redirect('drama:detail', question_id=question.id)
@@ -77,6 +79,7 @@ def question_modify(request, question_id):
         form = QuestionForm(instance=question)
     context = {'form': form}
     return render(request, 'drama/question_form.html', context)
+
 
 @login_required(login_url='common:login')
 def question_delete(request, question_id):
