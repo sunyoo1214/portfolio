5089947fbf6734f3d9fb9a881fcada44c9421bfa
sunyoo1214 <sunyoo1214@gmail.com>
2022-01-27 10:42:53 +0900

    3-5 로그인 템플릿 할 차례

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'drama.apps.DramaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
